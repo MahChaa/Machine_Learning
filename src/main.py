d2039d8fb0b8b5a6ef1435db10ac54e908a6e5ea
Mahdi Chaari <mahdi-chaari@hotmail.com>
2020-06-14 20:09:34 -0400

    -Optimized the algorithm used to find the first occurrence of a 2019 post in the data.

--- src/main.py
+++ src/main.py
@@ -9,14 +9,13 @@ import pandas as pd
 def main():
     print("Hello and welcome!")
 
-    # We skip the first column when creating the DataFrame since Pandas generates its own indexes for all elements
+    # We skip the first column when creating the DataFrame since Pandas generates its own indices for all elements
     data = pd.read_csv("../resources/hns_2018_2019.csv", usecols=range(1, 10))
 
-    # We iterate through the DataFrame until we come across the first 2019 entry, we save its index for future purposes
-    for index, row in data.iterrows():
-        if row["Created At"][:4] == "2019":
-            test_data_index_start = index
-            break
+    # We find the index of the first occurrence of the year 2019 in the Created At column assuming the data is sorted
+    test_sample_start_index = data["Created At"].values.searchsorted('2019', side='right')
+
+    print(test_sample_start_index)
 
 
 if __name__ == "__main__":
