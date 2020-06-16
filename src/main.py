2bc4e0c4d776d3badd9472ee9490df9454e0a111
Mahdi Chaari <mahdi-chaari@hotmail.com>
2020-06-15 20:51:59 -0400

    -Created a classes package in the src folder where to store new classes that will be used. -Simplified the main() method to outsource to the classes any code that is class related. -Created a Dataset class that will encapsulate all the data and required operations operations related to that data. -dataset.py now includes a function that will generate a model in the format required by the assignment. -Used nltk's word_tokenize instead of str.split() for extracting word tokens from the raw text.

--- src/main.py
+++ src/main.py
@@ -3,19 +3,15 @@
 # Written by Mahdi Chaari - 27219946
 # For COMP 472 Section ABKX – Summer 2020
 # --------------------------------------------------------
-import pandas as pd
+from classes.dataset import DataSet
 
 
 def main():
     print("Hello and welcome!")
 
-    # We skip the first column when creating the DataFrame since Pandas generates its own indices for all elements
-    data = pd.read_csv("../resources/hns_2018_2019.csv", usecols=range(1, 10))
+    data = DataSet("hns_2018_2019.csv", "2019")
 
-    # We find the index of the first occurrence of the year 2019 in the Created At column assuming the data is sorted
-    test_sample_start_index = data["Created At"].values.searchsorted('2019', side='right')
-
-    print(test_sample_start_index)
+    data.create_model("model-2018.txt")
 
 
 if __name__ == "__main__":
